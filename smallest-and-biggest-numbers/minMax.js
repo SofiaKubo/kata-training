a6272f78e57cde6284f391194f087bfaa5f9a1ca
SofiaKubo <skuboreva@gmail.com>
2025-12-01 14:52:50 +0100

    refactor(minMax): rewrite solution using optimized for-loop implementation

--- smallest-and-biggest-numbers/minMax.js
+++ smallest-and-biggest-numbers/minMax.js
@@ -9,4 +9,16 @@ function minMax(arr) {
   return [minElement, maxElement];
 }
 
+// Optimized O(n) solution using a standard for loop
+function minMax(arr) {
+  let minElement = arr[0];
+  let maxElement = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < minElement) minElement = arr[i];
+    if (arr[i] > maxElement) maxElement = arr[i];
+  }
+  return [minElement, maxElement];
+}
+
 module.exports = minMax;
